0c37bf5f195de2b4431379624ab5f9192a4b7353
Anton Holovin <unfeir@gmail.com>
2022-12-22 20:38:43 +0200

    v1

--- bot_assistant/main.py
+++ bot_assistant/main.py
@@ -1,4 +1,5 @@
 from classes import AddressBook
+from colorama import Fore, Style
 
 
 """
@@ -10,10 +11,18 @@ save_data, search, good_bye, close, exit, .)
 PHONE_BOOK = AddressBook()
 
 
+
+
 def helps():
-    return f"Команди на які відповідає помічник: \n"\
-           "help\n"\
-           "good_bye, close, exit, .\n"
+    commands = [f'{Fore.GREEN}add{Style.RESET_ALL} - will adding new contact to you addressbook in format add: [Name][Phone]',
+           f'{Fore.GREEN}change{Style.RESET_ALL} - will change one of you contact. format for change: [Name][Phone][New phone]',
+           f'{Fore.GREEN}delete{Style.RESET_ALL} - will delete contact. format [name]',
+           f'{Fore.GREEN}phone{Style.RESET_ALL} - will show all phone numbers of your contacts. format [name]',
+           f'{Fore.GREEN}upcoming_birthday{Style.RESET_ALL} - will show you upcoming Bday in  "n" days. format [quantity of days]',
+           f'{Fore.GREEN}save{Style.RESET_ALL} - will save you addressbook',
+           f'{Fore.GREEN}load{Style.RESET_ALL} - will load you addressbook']
+
+    return '\n'.join(commands)
 
 
 USER_COMMANDS = {
