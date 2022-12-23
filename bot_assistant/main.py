f10a873db3196b4ae334f51807be6b621487e9b7
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-23 10:13:53 +0200

    Update main.py

--- bot_assistant/main.py
+++ bot_assistant/main.py
@@ -13,13 +13,7 @@ def input_error(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except IndexError:
-            return "Try again, please"
-        except ValueError:
-            return "Try again, please"
-        except TypeError:
-            return "Try again, please"
-        except KeyError:
+        except (IndexError, ValueError, TypeError, KeyError):
             return "Try again, please"
 
     return inner
